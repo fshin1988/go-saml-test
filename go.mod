fdebfbe9dd6e8f9e57dc0de9e25a9d4b71ffd977
fshin1988 <noalv88@gmail.com>
2019-08-06 17:35:40 +0900

    Use saml library

--- go.mod
+++ go.mod
@@ -1,3 +1,12 @@
 module github.com/fshin1988/go-saml-test
 
 go 1.12
+
+require (
+	github.com/beevik/etree v1.1.0 // indirect
+	github.com/crewjam/saml v0.0.0-20190521120225-344d075952c9 // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/jonboulle/clockwork v0.1.0 // indirect
+	github.com/russellhaering/goxmldsig v0.0.0-20180430223755-7acd5e4a6ef7 // indirect
+	golang.org/x/crypto v0.0.0-20190701094942-4def268fd1a4 // indirect
+)
--- main.go
+++ main.go
@@ -1,16 +1,48 @@
 package main
 
 import (
+	"crypto/rsa"
+	"crypto/tls"
+	"crypto/x509"
 	"fmt"
 	"net/http"
+	"net/url"
+
+	"github.com/crewjam/saml/samlsp"
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, World!")
+	fmt.Fprintf(w, "Hello, %s!", samlsp.Token(r.Context()).Attributes.Get("cn"))
 }
 
 func main() {
+	keyPair, err := tls.LoadX509KeyPair("myservice.cert", "myservice.key")
+	if err != nil {
+		panic(err) // TODO handle error
+	}
+	keyPair.Leaf, err = x509.ParseCertificate(keyPair.Certificate[0])
+	if err != nil {
+		panic(err) // TODO handle error
+	}
+
+	idpMetadataURL, err := url.Parse("https://www.testshib.org/metadata/testshib-providers.xml")
+	if err != nil {
+		panic(err) // TODO handle error
+	}
+
+	rootURL, err := url.Parse("http://localhost:8000")
+	if err != nil {
+		panic(err) // TODO handle error
+	}
+
+	samlSP, _ := samlsp.New(samlsp.Options{
+		URL:            *rootURL,
+		Key:            keyPair.PrivateKey.(*rsa.PrivateKey),
+		Certificate:    keyPair.Leaf,
+		IDPMetadataURL: idpMetadataURL,
+	})
 	app := http.HandlerFunc(hello)
-	http.Handle("/hello", app)
+	http.Handle("/hello", samlSP.RequireAccount(app))
+	http.Handle("/saml/", samlSP)
 	http.ListenAndServe(":8000", nil)
 }
